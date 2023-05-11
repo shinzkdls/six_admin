803d3dfc5f9bd60a8925b5b2026888220ab895de
shinzkdls <shinzkdls@gmail.com>
2023-05-11 15:26:08 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/SixAdminApplication.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixAdminApplication.class, args);
+    }
+
+}
