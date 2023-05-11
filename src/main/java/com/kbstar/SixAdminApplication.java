27fd485e24f3dd407029e20ac6bc8f18c365138b
shinzkdls <shinzkdls@gmail.com>
2023-05-11 16:55:48 +0900

    six_admin 0511

--- src/main/java/com/kbstar/SixAdminApplication.java
+++ src/main/java/com/kbstar/SixAdminApplication.java
@@ -2,7 +2,11 @@ package com.kbstar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableScheduling
 @SpringBootApplication
 public class SixAdminApplication {
 
@@ -10,4 +14,11 @@ public class SixAdminApplication {
         SpringApplication.run(SixAdminApplication.class, args);
     }
 
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
